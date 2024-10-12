edc30a7078aed68c054ba2cd85b344f7635542e0
bqx <1142634487@qq.com>
2024-10-12 11:20:11 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <div id="CesiumContainer"></div>
+  </div>
+</template>
+
+<script setup>
+import * as Cesium from 'cesium'
+import 'cesium/Source/Widgets/widgets.css'
+
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  let viewer = new Cesium.Viewer('CesiumContainer')
+  console.log(viewer)
+})
+</script>
+
+<style lang="less" scoped>
+.home, #CesiumContainer {
+  width: 100%;
+  height: 100%;
+}
+</style>
